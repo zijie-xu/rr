a419de3b5bd6a2c68ac61762b80ad0da02326005
zijie-xu <65324598+zijie-xu@users.noreply.github.com>
2024-02-18 17:02:09 +0800

    Update rr2.py

--- rr2.py
+++ rr2.py
@@ -19,7 +19,7 @@ st.write(':blue[Only] :blue[application] :blue[to] :red[R16,R17,R18] :blue[Tires
 rrmodelfile ='static/1.dat'
 #rrmodelfile = '\\10.97.1.43\赛轮集团股份有限公司\技术研发中心\研发实验中心\技术研发中心实验系统\1.中心实验室\1.中心实验室\9-实验中心-成品组\03 六分力组\5、数据处理\rr\R16_R17_R18_M8(104,10).dat'
 st.write(rrmodelfile)
-load_model=pickle.load(open(rrmodelfile,"rb"))
+load_model=pickle.load(open(rrmodelfile))
 
 rr0=0
 with st.container():
